7e6ea55ad048a995fd1593e10367eba372a60bb1
Prashant Sharma <sharmaprashant217@gmail.com>
2025-02-26 18:51:32 +0530

    second commit

---
+++ src/content_option.js
@@ -0,0 +1,173 @@
+import portfolioImg from "./assets/images/portfolio.jpg";
+
+const logotext = "Prashant";
+const meta = {
+  title: "Prashant Sharma",
+  description: "I’m Prashant Sharma React Native Developer",
+};
+
+const introdata = {
+  title: "I’m Prashant Sharma",
+  animated: {
+    first: "I love coding",
+    second: "I code cool mobile apps",
+  },
+  description:
+    "A passionate React Native developer with a keen interest in building innovative mobile applications. I love coding, problem-solving, and continuously exploring new technologies.",
+  your_img_url: portfolioImg,
+};
+
+const dataabout = {
+  title: "abit about my self",
+  aboutme:
+    "I'm Prashant Sharma, a passionate software developer with a strong focus on React Native and mobile app development. I enjoy building innovative solutions, solving complex problems, and continuously learning new technologies. Currently, I'm exploring new opportunities to grow as a developer and contribute to impactful projects.",
+};
+const worktimeline = [
+  {
+    jobtitle: "React Native Developer",
+    where: "Talentrise Technokrateb Pvt. Ltd",
+    date: "2020",
+  },
+];
+
+const skills = [
+  {
+    name: "React Native",
+    value: 90,
+  },
+  {
+    name: "HTML",
+    value: 85,
+  },
+  {
+    name: "CSS",
+    value: 50,
+  },
+  {
+    name: "JavsScript",
+    value: 50,
+  },
+  {
+    name: "Flutter",
+    value: 60,
+  },
+  {
+    name: "Dart",
+    value: 50,
+  },
+];
+
+const services = [
+  {
+    title: "Mobile App Development",
+    description:
+      "Crafting seamless and innovative mobile applications using the latest technologies to enhance user experience, boost performance, and provide reliable functionality. Our approach focuses on creating intuitive designs, optimizing performance across multiple devices, and ensuring a smooth, engaging user interface. Whether it's a cross-platform or native app, we leverage the power of advanced tools to deliver high-quality solutions that meet both business goals and user expectations.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "sharmaprashant217@gmail.com",
+  YOUR_FONE: "(+91) 8791959426",
+  description:
+    "Feel free to reach out for any inquiries or collaborations. I'm always happy to connect!",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/sharmaprashant217",
+  linkedin: "https://www.linkedin.com/in/prashant-sharma-b2b69a1b1/",
+  instagram:
+    "https://www.instagram.com/who.prashantsharma?igsh=MW81MnRxZG43cmhvZA==",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
